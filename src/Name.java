18bb8d1a0a2d53f52c94b4c3820623fdb2e81968
LiChi <119381598+lchiDwight@users.noreply.github.com>
2023-01-17 13:55:53 +0900

    LC

--- src/Name.java
+++ src/Name.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
    public class Name {
+      
+//       keep 1 main function in the Main file, give a different name for this one
        public static void main (String[] args){
 
             System.out.println("What is your name?");
