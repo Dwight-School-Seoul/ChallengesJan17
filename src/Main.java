9144ef2ae6cda08bf46f83b550da938030c5ab4d
AZVR3 <jongyul3213@gmail.com>
2023-01-17 14:13:12 +0900

    Revert "Jongyul's work"
    This reverts commit d1d245a97f9b2aefa338a2040c27d680cd6e87a7.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        functions.hello();
-        functions.lifeInWeeks();
-        functions.BMICalculator();
-        functions.tipCalculator();
-        functions.characterCounter();
-        functions.magicEightBall();
+        System.out.println("Hello world!");
     }
 }
