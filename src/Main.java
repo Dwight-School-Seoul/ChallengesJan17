d5a1af9b70d693e07647b58a617df53cea344e59
MohammadAlhubail <2200180@dwight.or.kr>
2023-01-18 18:01:41 +0900

    Mohammad

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
-import javax.swing.JOptionPane;
+import javax.swing.*;
+
+import static javax.swing.JOptionPane.ERROR_MESSAGE;
+import static javax.swing.JOptionPane.PLAIN_MESSAGE;
+
 public class Main {
     public static void main(String[] args) {
 
-        String name = JOptionPane.showInputDialog("what is your name? ");
-        String age = JOptionPane.showInputDialog("How old are you?");
-        String color = JOptionPane.showInputDialog("What is your favourite color? ");
-        String doing = JOptionPane.showInputDialog("What do you want to ");
+        while(true) {
+            String name = JOptionPane.showInputDialog(null, "what is your name?", "NAME", PLAIN_MESSAGE);
+            String ageStr = JOptionPane.showInputDialog(null, "How old are you?", "AGE", PLAIN_MESSAGE);
+//            int age = Integer.parseInt(ageStr);
+            JOptionPane.showMessageDialog(null, "Hello " + name + ",\nYou are " + ageStr + " years old.", "Intro", PLAIN_MESSAGE);
+
+            int Option = JOptionPane.showConfirmDialog(null,"Try Again?","Again?",JOptionPane.YES_NO_CANCEL_OPTION,PLAIN_MESSAGE);
+
+            if(Option == 0){
+                continue;
+            }else{
+                return;
+            }
+        }
+
+
+
 
     } //void main
 } // main Class
