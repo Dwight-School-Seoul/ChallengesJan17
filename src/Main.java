3227a30d398065acbdb820fa59180543d0e2b666
hubailmn <76243856+hubailmn@users.noreply.github.com>
2023-02-21 14:33:32 +0900

    Mohammad

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-
-        Formative.question1();
-        Formative.question2();
-        Formative.question3();
-        Formative.question4();
-
+        ArraysPractice.sortPrintFromUser();
 
     }
 }
