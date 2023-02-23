2cef6a7de43c920b4f3e5b409bbb5f7e819c2e75
hubailmn <76243856+hubailmn@users.noreply.github.com>
2023-02-23 17:11:02 +0900

    Mohammad

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,7 @@
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        java.lang.StringBuilder myString = new java.lang.StringBuilder();
-        myString.append("hello this is dwight school seoul");
-
-        // Find the index of the word "this"
-        int index = myString.indexOf("this");
-        System.out.println("Index of 'this': " + index);
-
-        // Replace "hello" with "Hi"
-        myString.replace(0, 5, "Hi");
-        System.out.println(myString);
-
-        // Delete the word "school"
-        int startIndex = myString.indexOf("school");
-        int endIndex = startIndex + "school".length();
-        myString.delete(startIndex, endIndex);
-        System.out.println(myString);
+        StrBuilder.stringBuild();
 
     }
 }
--- src/StrBuilder.java
+++ src/StrBuilder.java
@@ -1,4 +1,4 @@
-public class StringBuilder {
+public class StrBuilder {
 
     public static void stringBuild(){
 
